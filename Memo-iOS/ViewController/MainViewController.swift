4920b03deaee81d7c34ab053cb8d74c89a58e8c1
JaekyungYou <youjk96@gmail.com>
2021-03-10 12:03:46 +0900

    feat: create model and MainViewController

--- Memo-iOS/ViewController/MainViewController.swift
+++ Memo-iOS/ViewController/MainViewController.swift
@@ -10,20 +10,53 @@ import UIKit
 
 class MainViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    let dateFormatter: DateFormatter = {
+       let df = DateFormatter()
+        df.dateStyle = .long
+        df.timeStyle = .short
+        df.locale = Locale(identifier: "KO_kr")
+        return df
+        
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.layer.cornerRadius = 10
+        view.backgroundColor = #colorLiteral(red: 0.9184874892, green: 0.9130274653, blue: 0.9226844907, alpha: 1)
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        navigationItem.searchController = searchController
+        
+        
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if scrollView.panGestureRecognizer.translation(in: scrollView).y < 0 {
+            navigationController?.setNavigationBarHidden(true, animated: true)
+        } else {
+            navigationController?.setNavigationBarHidden(false, animated: true)
+        }
     }
 }
-/*
 
 extension MainViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return Memo.dummyMemoList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MemoCell", for: indexPath)
+        
+        let memo = Memo.dummyMemoList[indexPath.row]
+        cell.textLabel?.text = memo.content
+        cell.detailTextLabel?.text = dateFormatter.string(from: memo.insertDate)
+        
+        return cell
     }
     
 }
-*/
+
